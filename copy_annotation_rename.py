0b3f8ab97fa01526d62e881f2b679206eaa56812
raju1207gupta <54761123+raju1207gupta@users.noreply.github.com>
2021-06-21 09:25:15 +0530

    Update copy_annotation_rename.py

--- copy_annotation_rename.py
+++ copy_annotation_rename.py
@@ -19,8 +19,8 @@ def xml_file_rename(path,path_to_final_annotation):
             root.find('filename').text = f"{frmnumber}.jpg"
             tree.write(path_to_final_annotation+f"{frmnumber}.xml")
 
-path_to_final_annotation  = 'D:/deeplearning learn/Social distancing/face-mask-detector/final_dataset_images/final_dataset_annotation/'
-PATH_TO_ANNOTATION = "D:/deeplearning learn/Social distancing/face-mask-detector/dataset_HUL_annotate1_person/annotations/"
+path_to_final_annotation  = 'path_to/final_dataset_annotation/'
+PATH_TO_ANNOTATION = "path_to/annotations/"
 
 xml_file_rename(PATH_TO_ANNOTATION,path_to_final_annotation)
         
